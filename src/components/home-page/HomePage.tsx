d4e31c47159b0c448403cf8040b2fc23b9c093a7
Hemenhk <hemen.hiwakamal@hotmail.com>
2023-10-20 09:44:51 +0200

    create a hero section

--- src/components/home-page/HomePage.tsx
+++ src/components/home-page/HomePage.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import TheHero from './hero/TheHero'
 
 export default function HomePage() {
   return (
-    <div className='flex justify-center items-center'>HomePage</div>
+    <div className='flex justify-center items-center lg:justify-start'>
+      <TheHero />
+    </div>
   )
 }
--- src/components/home-page/hero/TheHero.tsx
+++ src/components/home-page/hero/TheHero.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 export default function TheHero() {
   return (
-    <div>TheHero</div>
-  )
+    <div className={`flex flex-col justify-center items-center lg:items-start pl-10 w-[60%] gap-5 text-center lg:text-left`}>
+      <p className="tracking-wider text-white text-lg font-semibold uppercase">
+        Välkommen till PersiCare Beauty
+      </p>
+      <p className="tracking-wider uppercase text-2xl font-medium text-[#dbdbdb]">
+        Din klinik för skönhet & hälsa i Stockholm, Solna
+      </p>
+      <Button variant={"outline"} className="uppercase tracking-wider w-52 h-12">Boka tid</Button>
+    </div>
+  );
 }
